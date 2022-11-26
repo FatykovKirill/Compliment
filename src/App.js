7fcd1c0288218269053693a317255eb36b3c25d5
Fatykov Kirill <fatykovkirill@gmail.com>
2022-11-26 12:16:44 +0500

    creat project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.styles.scss';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <h1>Compliment</h1>
+      <button>Log in</button>
+      <button>Sign up</button>
+    </div>
+
+  )
+}
+
+export default App;
